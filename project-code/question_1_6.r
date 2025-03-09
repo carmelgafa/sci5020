e7292ba4fe4292108a740319d195b017b9958afc
carmelgafa <carmelgafa@gmail.com>
2025-03-09 03:57:53 +0100

    question 1 vi

--- project-code/question_1_6.r
+++ project-code/question_1_6.r
@@ -1,4 +1,4 @@
-# Question 1.5
+# Question 1.6
 # Generate 1000 points from an exponential distributed random variable
 # with rate parameter λ = 1.5.
 # Plot the histogram of the exponential random variable with lambda = 1.5
@@ -36,3 +36,32 @@ p <- ggplot(data,
 quartz()
 print(p)
 
+
+LL_exponential <- function(lambda, x) {
+  n <- length(x)
+  log_likelihood <- n * log(lambda) - lambda * sum(x)
+  return(log_likelihood)
+}
+
+lambda_values <- seq(0.1, 5, by = 0.01)
+log_likelihood_values <- sapply(lambda_values,
+                                function(lambda) LL_exponential(lambda, x))
+
+df <- data.frame(lambda_values, log_likelihood_values)
+
+p <- ggplot(df, aes(x = lambda_values, y = log_likelihood_values)) +
+  geom_point(
+             color = "blue",
+             alpha = 0.6) +
+  labs(
+       title = "Log-Likelihood for with varying lambda values",
+       x = "Lambda",
+       y = "Log-Likelihood") +
+  theme_bw()
+
+quartz()
+print(p)
+
+max_ll <- max(log_likelihood_values)
+max_lambda <- lambda_values[log_likelihood_values == max_ll]
+print(glue("Lambda value for maximum log-likelihood is {max_lambda}"))
