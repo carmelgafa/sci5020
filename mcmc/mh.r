c3360666d4eb73f44376fe6d5482ee5d8cee3885
carmelgafa <carmelgafa@gmail.com>
2025-04-09 18:20:29 +0200

    mcmc - added gibbs 1

--- mcmc/mh.r
+++ mcmc/mh.r
@@ -10,7 +10,7 @@ plot(h,f,type='l')
 target<-function(x){(sin(x)^2)*(sin(2*x)^2)*dnorm(x)}
 
 #creating the Metropolis Hastings function
-metropolis<-function(x,alpha=3){
+metropolis<-function(x,alpha=0.3){
   #generate y from uniform distribution around (x-alpha, x+alpha)
   y=runif(1,x-alpha,x+alpha)
   #if U is greater than alpha(x,y) then keep the current x by putting into y
@@ -44,14 +44,14 @@ print(acceptanceprob)
 #trace plot
 plot(x,type='l')
 
-#taking a burn-in of 10%
-burnin<-round(0.1*N,0)
+# #taking a burn-in of 10%
+# burnin<-round(0.1*N,0)
 
-#plotting EPDF plot minus the burn-in period
-install.packages("EnvStats")
-library(EnvStats)
-epdfPlot(x[(burnin+1):length(N)],main="Empirical PDF",xlab='x')
+# #plotting EPDF plot minus the burn-in period
+# install.packages("EnvStats")
+# library(EnvStats)
+# epdfPlot(x[(burnin+1):length(N)],main="Empirical PDF",xlab='x')
 
-#plotting histogram minus the burn-in period
-hist(x[(burnin+1):length(N)], main="Histogram", xlab='x')
+# #plotting histogram minus the burn-in period
+# hist(x[(burnin+1):length(N)], main="Histogram", xlab='x')
 
